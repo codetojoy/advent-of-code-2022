0cf158cda42d3ec89910f81f4f597d61b7594483
Michael Easter <codetojoy@gmail.com>
2022-12-07 06:56:57 -0400

    update Runner

--- day5/part1/groovy-gradle/src/main/groovy/net/codetojoy/Runner.groovy
+++ day5/part1/groovy-gradle/src/main/groovy/net/codetojoy/Runner.groovy
@@ -3,7 +3,20 @@ package net.codetojoy
 
 class Runner {
     static void main(def args) {
-        println "TRACER arg1: " + args[0] 
+        def inputFile = new File(args[0])
+        assert inputFile.exists()
+        
+        def inputLines = (inputFile as List).collect { it }
+
+        def stacks = new StacksBuilder().buildStacks(inputLines)
+        def moves = new MovesBuilder().buildMoves(inputLines)
+        stacks.applyMoves(moves)
+
+        def tops = stacks.getTops()
+        println "TRACER tops: "
+        tops.each { print it }
+        println ""
+
         println "Ready."
     }
 }
