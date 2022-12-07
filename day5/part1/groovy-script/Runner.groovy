a484861c178026702baafe2316e272f3e240840e
Michael Easter <codetojoy@gmail.com>
2022-12-07 06:36:28 -0400

    rename dirs

--- day5/part1/groovy-script/Runner.groovy
+++ day5/part1/groovy-script/Runner.groovy
@@ -1,6 +1,13 @@
 
 import groovy.transform.ToString
 
+@ToString 
+class MoveInfo {
+    def n
+    def from
+    def to
+}
+
 @ToString
 class ConfigInfo {
     // e.g. [2] -> 'M'
@@ -24,6 +31,21 @@ class ConfigInfo {
 class Stacks {
     def stacks = [:].withDefault { key -> new ArrayDeque<String>() }
 
+    def getTops() {
+        def results = []
+        def keys = stacks.keySet().sort()
+        println "TRACER keys: " + keys
+        keys.each { key -> 
+            def stack = getStack(key)
+            if (stack.isEmpty()) {
+                results << " ERROR "
+            } else {
+                results << stack.peekFirst()
+            }
+        }
+        return results
+    }
+
     def addFirst(index, item) {
         def stack = stacks[index]
         stack.addFirst(item)
@@ -34,12 +56,26 @@ class Stacks {
         stacks[index]
     }
 
-    def apply(configInfo) {
+    def applyConfig(configInfo) {
         configInfo.keys().each { key ->
             def value = configInfo.get(key)
             stacks[key].addLast(value)
         }
         this
+    }
+
+    def applyMove(moveInfo) {
+        def n = moveInfo.n
+        def src = getStack(moveInfo.from)
+        def dest = getStack(moveInfo.to)
+        n.times { 
+            def crate = src.removeFirst()
+            dest.addFirst(crate)
+        }
+    }
+
+    def applyMoves(moves) {
+        moves.each { applyMove(it) }
     }
 }
 
@@ -50,17 +86,38 @@ def testConfigInfo1 = new ConfigInfo()
 testConfigInfo1.put(1, 'A')
 testConfigInfo1.put(2, 'B')
 
-assert 1 == new Stacks().apply(testConfigInfo1).getStack(1).size()
-assert 1 == new Stacks().apply(testConfigInfo1).getStack(2).size()
+assert 1 == new Stacks().applyConfig(testConfigInfo1).getStack(1).size()
+assert 1 == new Stacks().applyConfig(testConfigInfo1).getStack(2).size()
 
 def testStacks = new Stacks()
-testStacks.apply(testConfigInfo1)
+testStacks.applyConfig(testConfigInfo1)
 def testConfigInfo2 = new ConfigInfo()
 testConfigInfo2.put(2, 'C')
 
-assert 2 == testStacks.apply(testConfigInfo2).getStack(2).size()
-assert 'B' == testStacks.apply(testConfigInfo2).getStack(2).peekFirst()
-assert 'C' == testStacks.apply(testConfigInfo2).getStack(2).peekLast()
+assert 2 == testStacks.applyConfig(testConfigInfo2).getStack(2).size()
+assert 'B' == testStacks.applyConfig(testConfigInfo2).getStack(2).peekFirst()
+assert 'C' == testStacks.applyConfig(testConfigInfo2).getStack(2).peekLast()
+
+def testMoves() {
+    def stacks = new Stacks()
+    stacks.applyConfig(new ConfigInfo().put(1, 'A'))
+    stacks.applyConfig(new ConfigInfo().put(1, 'B'))
+    stacks.applyConfig(new ConfigInfo().put(1, 'C'))
+    stacks.applyConfig(new ConfigInfo().put(2, 'D'))
+    stacks.applyConfig(new ConfigInfo().put(2, 'E'))
+    stacks.applyConfig(new ConfigInfo().put(2, 'F'))
+    def moveInfo = new MoveInfo(n: 3, from: 1, to: 2)
+    
+    // test
+    stacks.applyMove(moveInfo)
+
+    assert 6 == stacks.getStack(2).size()
+    assert 'C' == stacks.getStack(2).peekFirst()
+    assert 'F' == stacks.getStack(2).peekLast()
+    assert 0 == stacks.getStack(1).size()
+}
+
+testMoves()
 
 def isConfigLine(line) {
     def regex = /.*\[[A-Z]\].*/
@@ -131,7 +188,7 @@ def buildStacks(lines) {
 
         if (isConfigLine(line)) {
             def configInfo = parseConfigLine(line)
-            stacks.apply(configInfo)
+            stacks.applyConfig(configInfo)
         } else {
             isDone = true
         }
@@ -140,6 +197,30 @@ def buildStacks(lines) {
     }
 
     stacks
+}
+
+
+def buildMove(line) {
+    def result = null
+    def regex = /.*move (\d+) from (\d+) to (\d+).*/
+    def matcher = (line =~ regex)
+
+    if (matcher.matches()) {
+        result = new MoveInfo()        
+        result.n = matcher[0][1] as int
+        result.from = matcher[0][2] as int
+        result.to = matcher[0][3] as int
+    }
+
+    result
+}
+
+assert 1 == buildMove('move 1 from 7 to 4').n
+assert 7 == buildMove('move 1 from 7 to 4').from
+assert 4 == buildMove('move 1 from 7 to 4').to
+
+def buildMoves(lines) {
+    lines.findResults { buildMove(it) }
 }
 
 def isTest = false
@@ -154,12 +235,14 @@ if (isTest) {
 
 def inputLines = (new File("input.txt") as List).collect { it } 
 def stacks = buildStacks(inputLines)
-println "TRACER stacks: " + stacks
-/*
-def overlaps = findOverlaps(inputLines)
-def total = overlaps.size()
-println "total: " + total
-*/
+// println "TRACER stacks: " + stacks
+def moves = buildMoves(inputLines)
+// println "TRACER moves: " + moves
+stacks.applyMoves(moves)
+def tops = stacks.getTops()
+println "TRACER tops: "
+tops.each { print it }
+println "" 
 
 println "Ready."
 
