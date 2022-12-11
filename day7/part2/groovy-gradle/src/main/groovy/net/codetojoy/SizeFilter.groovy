eaaf4aa6ce0001822b590072fcf306f5e26e1443
Michael Easter <codetojoy@gmail.com>
2022-12-11 01:00:01 -0400

    remove static

--- day7/part2/groovy-gradle/src/main/groovy/net/codetojoy/SizeFilter.groovy
+++ day7/part2/groovy-gradle/src/main/groovy/net/codetojoy/SizeFilter.groovy
@@ -2,7 +2,7 @@
 package net.codetojoy
 
 class SizeFilter {
-    static def findTotal(pathMap, threshold) {
+    def findTotal(pathMap, threshold) {
         def total = pathMap.findAll { path, sizeInfo ->
                         (sizeInfo.size <= threshold)
                     }.collect { path, sizeInfo ->
@@ -11,7 +11,7 @@ class SizeFilter {
         total
     }
 
-    static def findDirForDeletion(pathMap, maxSize, targetSize) {
+    def findDirForDeletion(pathMap, maxSize, targetSize) {
         def rootSize = pathMap['/'].size
         def freeSize = maxSize - rootSize
         def targetDelta = targetSize - freeSize
